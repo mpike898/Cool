4225435841130cd7b5b141290515a393f9de2bd4
Matt Pike <mpike_88@hotmail.com>
2020-07-30 13:59:58 -0600

    added Page Objects

---
+++ pageObjects/yooPage.js
@@ -0,0 +1,82 @@
+module.exports = {
+    url: 'https://yoodlize.com',
+    elements: {
+    
+
+
+
+
+
+    seeAll: {
+        selector: '//*[contains(@class,"fmVgeN")]',
+        locateStrategy: 'xpath'
+    },
+    seeAll1: {
+        selector: '(//div[contains(@class,"fmVgeN")])[1]',
+        locateStrategy: 'xpath'
+    },
+    seeAll2: {
+        selector: '(//div[contains(@class,"fmVgeN")])[2]',
+        locateStrategy: 'xpath'
+    },
+    seeAll3: {
+        selector: '(//div[contains(@class,"fmVgeN")])[3]',
+        locateStrategy: 'xpath'
+    },
+    seeAll4: {
+        selector: '(//div[contains(@class,"fmVgeN")])[4]',
+        locateStrategy: 'xpath'
+    },
+    seeAll5: {
+        selector: '(//div[contains(@class,"fmVgeN")])[5]',
+        locateStrategy: 'xpath'
+    },
+    seeAll6: {
+        selector: '(//div[contains(@class,"fmVgeN")])[6]',
+        locateStrategy: 'xpath'
+    },
+    seeAll7: {
+        selector: '(//div[contains(@class,"fmVgeN")])[7]',
+        locateStrategy: 'xpath'
+    },
+    seeAll8: {
+        selector: '(//div[contains(@class,"fmVgeN")])[8]',
+        locateStrategy: 'xpath'
+    },
+    seeAll9: {
+        selector: '(//div[contains(@class,"fmVgeN")])[9]',
+        locateStrategy: 'xpath'
+    },
+    seeAll10: {
+        selector: '(//div[contains(@class,"fmVgeN")])[10]',
+        locateStrategy: 'xpath'
+    },
+    seeAll11: {
+        selector: '(//div[contains(@class,"fmVgeN")])[11]',
+        locateStrategy: 'xpath'
+    },
+    seeAll12: {
+        selector: '(//div[contains(@class,"fmVgeN")])[12]',
+        locateStrategy: 'xpath'
+    },
+    seeAll13: {
+        selector: '(//div[contains(@class,"fmVgeN")])[13]',
+        locateStrategy: 'xpath'
+    },
+    seeAll14: {
+        selector: '(//div[contains(@class,"fmVgeN")])[14]',
+        locateStrategy: 'xpath'
+    },
+    seeAll15: {
+        selector: '(//div[contains(@class,"fmVgeN")])[15]',
+        locateStrategy: 'xpath'
+    },
+    verifyCata: {
+        selector: '//*[contains(@class,"jSqgxr")]',
+        locateStrategy: 'xpath'
+
+    }
+    
+    }
+    
+    }//*[contains(@class,"jSqgxr")]
